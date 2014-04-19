9eff40e1dbcf4154f668918e6267e42e8cf117e6
smartgamerT <narayan_menon@yahoo.com>
2014-04-19 16:11:33 -0700

    last  change

--- ObstacleCourse.java
+++ ObstacleCourse.java
@@ -17,8 +17,6 @@ public class ObstacleCourse extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(800, 600, 1); 
-
-        prepare();
     }
 
     /**
